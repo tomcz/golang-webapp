31fa68367b5b15ed278a23c1b5a08aab18c936b6
Tom Czarniecki <tomcz@users.noreply.github.com>
2023-04-19 18:44:49 +1000

    common redirect functions

--- internal/webapp/handlers/index.go
+++ internal/webapp/handlers/index.go
@@ -18,5 +18,5 @@ func updateIndex(w http.ResponseWriter, r *http.Request) {
 		s := webapp.CurrentSession(r)
 		s.AddFlashSuccess(fmt.Sprintf("Hello %s", name))
 	}
-	webapp.Redirect(w, r, "/index")
+	redirectToIndex(w, r)
 }
