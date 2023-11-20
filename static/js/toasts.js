ed06c50ea08fac07e8ea1b8ef98060b7897cff65
Tom Czarniecki <tomcz@users.noreply.github.com>
2023-11-20 14:38:34 +1100

    don't need jquery to clear toasts

--- static/js/toasts.js
+++ static/js/toasts.js
@@ -1,7 +1,6 @@
-document.querySelectorAll("div.toast button.btn-clear")
-  .forEach(function (btn) {
-    btn.addEventListener("click", function () {
-      btn.closest("div.toast").style.display = "none";
-      return false;
-    });
+document.querySelectorAll("div.toast button.btn-clear").forEach((btn) => {
+  btn.addEventListener("click", () => {
+    btn.closest("div.toast").style.display = "none";
+    return false;
   });
+});
