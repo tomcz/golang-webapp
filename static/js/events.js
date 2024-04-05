298700217ba5883691162560ba65b7e11b608d95
Tom Czarniecki <tomcz@users.noreply.github.com>
2024-04-05 12:59:36 +1100

    innerText converts line breaks

--- static/js/events.js
+++ static/js/events.js
@@ -11,25 +11,15 @@ document.addEventListener("htmx:afterRequest", function (evt) {
 });
 
 document.addEventListener("htmx:responseError", function (evt) {
-  const error = evt.detail.xhr.responseText.trim();
-
-  const pre = document.createElement("pre");
-  pre.classList.add("response-error");
-  pre.appendChild(document.createTextNode(error));
-
   const div = document.createElement("div");
+  div.innerText = evt.detail.xhr.responseText.trim()
   div.classList.add("toast", "toast-error");
-  div.appendChild(pre);
-
   evt.detail.target.replaceChildren(div);
 });
 
 document.addEventListener("htmx:sendError", function (evt) {
-  const error = "Network error, please try again or reload page.";
-
   const div = document.createElement("div");
+  div.innerText = "Network error, please try again or reload page.";
   div.classList.add("toast", "toast-error");
-  div.appendChild(document.createTextNode(error));
-
   evt.detail.target.replaceChildren(div);
 });
