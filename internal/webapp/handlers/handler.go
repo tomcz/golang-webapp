ffdcc607c4c93dd10bf1ac50a2f3385f7771980c
Tom Czarniecki <tomcz@users.noreply.github.com>
2024-10-29 12:12:12 -0400

    only match / and not /*

--- internal/webapp/handlers/handler.go
+++ internal/webapp/handlers/handler.go
@@ -10,8 +10,7 @@ func NewHandler(sw webapp.SessionWrapper, knownUsers map[string]string) http.Han
 	mux := http.NewServeMux()
 
 	// no session
-	mux.Handle("/", http.RedirectHandler("/index", http.StatusFound))
-	mux.Handle("/favicon.ico", http.NotFoundHandler())
+	mux.Handle("/{$}", http.RedirectHandler("/index", http.StatusFound))
 	mux.HandleFunc("/error", exampleError)
 	mux.HandleFunc("/panic", examplePanic)
 
