e7711d1f370a28c94ccc2232a5ab7377c74266b1
Tom Czarniecki <tomcz@users.noreply.github.com>
2025-10-27 08:51:43 +1100

    error handing in one place

--- internal/webapp/errors.go
+++ internal/webapp/errors.go
@@ -3,10 +3,25 @@ package webapp
 import (
 	"fmt"
 	"net/http"
+	"runtime/debug"
 )
 
 func HttpError(w http.ResponseWriter, r *http.Request, statusCode int, msg string, err error) {
 	RSet(r, "error", err)
 	msg = fmt.Sprintf("ID: %s\nError: %s\n", ReqID(r), msg)
 	http.Error(w, msg, statusCode)
+}
+
+func withPanicRecovery(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if p := recover(); p != nil {
+				stack := string(debug.Stack())
+				RSet(r, "panic_stack", stack)
+				err := fmt.Errorf("panic: %v", p)
+				HttpError(w, r, http.StatusInternalServerError, "Request failed", err)
+			}
+		}()
+		next.ServeHTTP(w, r)
+	})
 }
--- internal/webapp/routes.go
+++ internal/webapp/routes.go
@@ -7,7 +7,6 @@ import (
 	"io"
 	"net/http"
 	"net/url"
-	"runtime/debug"
 	"strings"
 	"time"
 
@@ -140,20 +139,6 @@ func logFormatter(_ io.Writer, p handlers.LogFormatterParams) {
 		logFunc = md.logger.Info
 	}
 	logFunc("request finished", fields...)
-}
-
-func withPanicRecovery(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		defer func() {
-			if p := recover(); p != nil {
-				stack := string(debug.Stack())
-				RSet(r, "panic_stack", stack)
-				err := fmt.Errorf("panic: %v", p)
-				HttpError(w, r, http.StatusInternalServerError, "Request failed", err)
-			}
-		}()
-		next.ServeHTTP(w, r)
-	})
 }
 
 func withCommit(commit string) mux.MiddlewareFunc {
