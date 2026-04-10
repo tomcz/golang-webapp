e09333ef877e73882a5b16d3a4853beb9dc1ec48
Tom Czarniecki <tomcz@users.noreply.github.com>
2026-04-10 22:43:04 +1000

    use kong custom types

--- cmd/webapp/main.go
+++ cmd/webapp/main.go
@@ -7,7 +7,7 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
-	"strings"
+	"reflect"
 	"time"
 
 	"github.com/alecthomas/kong"
@@ -22,17 +22,17 @@ import (
 var commit string
 
 type serviceCmd struct {
-	KnownUsers     string        `env:"KNOWN_USERS" help:"Valid 'user:password,user2:password2,...' combinations."`
-	LogLevel       string        `env:"LOG_LEVEL" default:"info" help:"Logging level (debug, info, warn, error)."`
-	LogType        string        `env:"LOG_TYPE" default:"colour" help:"Logger type (default, colour, text, json)."`
-	ListenAddr     string        `env:"LISTEN_ADDR" default:":3000" help:"Service 'ip:port' listen address."`
-	TlsCertFile    string        `env:"TLS_CERT_FILE" type:"existingfile" help:"For HTTPS service, optional."`
-	TlsKeyFile     string        `env:"TLS_KEY_FILE" type:"existingfile" help:"For HTTPS service, optional."`
-	SessionName    string        `env:"SESSION_NAME" default:"_app_session" help:"Name of session cookie."`
-	SessionMaxAge  time.Duration `env:"SESSION_MAX_AGE" default:"24h" help:"MaxAge of session cookie."`
-	SessionAuthKey string        `env:"SESSION_AUTH_KEY" help:"Session authentication key."`
-	SessionEncKey  string        `env:"SESSION_ENC_KEY" help:"Session encryption key."`
-	BehindProxy    bool          `env:"BEHIND_PROXY" help:"Use HTTP proxy headers."`
+	KnownUsers     map[string]string `env:"KNOWN_USERS" help:"Where KEY is the username and VALUE is the hashed password."`
+	LogLevel       slog.Level        `env:"LOG_LEVEL" type:"level" help:"Logging level (debug, info, warn, error)."`
+	LogType        string            `env:"LOG_TYPE" default:"colour" enum:"default,colour,text,json" help:"Logger type (default, colour, text, json)."`
+	ListenAddr     string            `env:"LISTEN_ADDR" default:"127.0.0.1:3000" help:"Service 'ip:port' listen address."`
+	TlsCertFile    string            `env:"TLS_CERT_FILE" placeholder:"FILE" type:"existingfile" help:"For HTTPS service, optional."`
+	TlsKeyFile     string            `env:"TLS_KEY_FILE" placeholder:"FILE" type:"existingfile" help:"For HTTPS service, optional."`
+	SessionName    string            `env:"SESSION_NAME" default:"_app_session" help:"Name of session cookie."`
+	SessionMaxAge  time.Duration     `env:"SESSION_MAX_AGE" default:"24h" help:"MaxAge of session cookie."`
+	SessionAuthKey string            `env:"SESSION_AUTH_KEY" help:"Session authentication key."`
+	SessionEncKey  string            `env:"SESSION_ENC_KEY" help:"Session encryption key."`
+	BehindProxy    bool              `env:"BEHIND_PROXY" help:"Use HTTP proxy headers."`
 }
 
 type keygenCmd struct{}
@@ -46,7 +46,12 @@ type appCfg struct {
 }
 
 func main() {
-	cfg := kong.Parse(&appCfg{}, kong.Description("Example golang webapp."))
+	opts := []kong.Option{
+		kong.Description("Example golang webapp."),
+		kong.NamedMapper("level", kong.MapperFunc(levelMapper)),
+		kong.HelpOptions{Compact: true},
+	}
+	cfg := kong.Parse(&appCfg{}, opts...)
 	if err := cfg.Run(); err != nil {
 		slog.Error("application failed", "error", err)
 		os.Exit(1)
@@ -78,7 +83,7 @@ func (a *serviceCmd) Run() error {
 	}
 	handler := app.Handler(app.HandlerConfig{
 		Sessions:    sessions,
-		KnownUsers:  a.parseKnownUsers(),
+		KnownUsers:  a.KnownUsers,
 		Commit:      commit,
 		BehindProxy: a.BehindProxy,
 	})
@@ -116,48 +121,38 @@ func (a *serviceCmd) runServer(server *http.Server, log *slog.Logger) error {
 	return err
 }
 
-func (a *serviceCmd) parseKnownUsers() map[string]string {
-	if a.KnownUsers == "" {
-		return nil
+func levelMapper(c *kong.DecodeContext, target reflect.Value) error {
+	var value string
+	err := c.Scan.PopValueInto("level", &value)
+	if err != nil {
+		return err
 	}
-	users := map[string]string{}
-	for token := range strings.SplitSeq(a.KnownUsers, ",") {
-		before, after, found := strings.Cut(token, ":")
-		if found {
-			users[before] = after
-		}
+	var level slog.Level
+	err = level.UnmarshalText([]byte(value))
+	if err != nil {
+		return err
 	}
-	return users
+	target.Set(reflect.ValueOf(level))
+	return nil
 }
 
 func (a *serviceCmd) setupLogging() *slog.Logger {
-	var level slog.Level
-	switch a.LogLevel {
-	case "debug":
-		level = slog.LevelDebug
-	case "warn":
-		level = slog.LevelWarn
-	case "error":
-		level = slog.LevelError
-	default:
-		level = slog.LevelInfo
-	}
 	logDefaults := []any{"build", commit}
 	switch a.LogType {
 	case "text":
-		opts := &slog.HandlerOptions{Level: level}
+		opts := &slog.HandlerOptions{Level: a.LogLevel}
 		h := slog.NewTextHandler(os.Stderr, opts)
 		slog.SetDefault(slog.New(h).With(logDefaults...))
 	case "json":
-		opts := &slog.HandlerOptions{Level: level}
+		opts := &slog.HandlerOptions{Level: a.LogLevel}
 		h := slog.NewJSONHandler(os.Stderr, opts)
 		slog.SetDefault(slog.New(h).With(logDefaults...))
 	case "colour":
-		opts := &tint.Options{Level: level, TimeFormat: time.TimeOnly, ReplaceAttr: highlightErrors}
+		opts := &tint.Options{Level: a.LogLevel, TimeFormat: time.TimeOnly, ReplaceAttr: highlightErrors}
 		h := tint.NewHandler(os.Stderr, opts)
 		slog.SetDefault(slog.New(h).With(logDefaults...))
 	default:
-		slog.SetLogLoggerLevel(level)
+		slog.SetLogLoggerLevel(a.LogLevel)
 		slog.SetDefault(slog.Default().With(logDefaults...))
 	}
 	return slog.With("component", "main")
